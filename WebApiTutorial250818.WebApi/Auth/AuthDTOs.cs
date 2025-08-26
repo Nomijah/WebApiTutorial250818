8e409848eb7ba5bb6efaa1c4f0959e05ebd612d0
Nomijah <petter.bostrom@gmail.com>
2025-08-26 09:05:24 +0200

    yesterday's lesson push

---
+++ WebApiTutorial250818.WebApi/Auth/AuthDTOs.cs
@@ -0,0 +1,6 @@
+﻿namespace WebApiTutorial250818.WebApi.Auth
+{
+    public record RegisterDto(string Email, string Password);
+    public record LoginDto(string Email, string Password);
+    public record AuthResponseDto(string AccessToken);
+}
